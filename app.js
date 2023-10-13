e7e9927e87d484019a4503de3051d4f31642dcd4
akarat <akrs33@gmail.com>
2023-10-13 13:50:46 +0700

    Update code

--- app.js
+++ app.js
@@ -17,69 +17,49 @@ console.log(process.env.DATABASE_URL)
 // console.log(pool)  
 
 // Define your API route for saving data.
-app.post('/save-data', (req, res) => {
+app.post('/save-data', async(req, res) => {
   // Handle saving the data to the PostgreSQL database here.
   // Parse the request body to get the data to save.
   const data = req.body;
 
   // Add code to save the data to the PostgreSQL database.
   // You'll need to establish a connection and use pg-promise or another PostgreSQL library to perform database operations.
+  let connection = await getConnection();
 
-    // Insert the data into the database.
-    // Use a connection from the pool to insert the data into the database.
-    pool.getConnection((err, connection) => {
-        if (err) {
-        console.log(err)
-            //print error to console for debugging purposes 
-            //print stack trace to console for debugging purposes 
+  const sqlquery = 'INSERT INTO dq_data (firstName, lastName, cardID, tel, email, year, month, day, sex, agreement1, agreement2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  const values = [
+    data.firstName,
+    data.lastName,
+    data.cardID,
+    data.tel,
+    data.email,
+    data.year,
+    data.month,
+    data.day,
+    data.sex,
+    data.agreement1,
+    data.agreement2,
+    ];
 
-
-        res.status(500).json({ error: 'Database connection error '+err+ ' ' +err.stack });
-        return;
-        }
-
-        const query = 'INSERT INTO dq_data (firstName, lastName, cardID, tel, email, year, month, day, sex, agreement1, agreement2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        const values = [
-        data.firstName,
-        data.lastName,
-        data.cardID,
-        data.tel,
-        data.email,
-        data.year,
-        data.month,
-        data.day,
-        data.sex,
-        data.agreement1,
-        data.agreement2,
-        ];
-
-
-        connection.query(query, values, (queryError) => {
-        connection.release(); // Release the connection back to the pool.
-
-        if (queryError) {
-            res.status(500).json({ error: 'An error occurred while saving data' });
+  const [rows] = await connection.query(sqlquery, values, (queryError) => {
+        if (error) {
+            return res.status(500).json({ error: 'An error occurred while saving data : '+ error });
         } else {
-            res.status(201).json({ message: 'Data saved successfully' });
+            return res.status(201).json({ message: 'Data saved successfully' });
         }
-        });
     });
+  connection.release();
 
   // Send a response to the client once data is saved.
   //res.status(201).json({ message: 'Data saved successfully' });
 });
 
 app.get('/get-data', async(req, res) => {
-    // res.status(201).json({ message: 'Data consume successfully' });
 
     let connection = await getConnection();
     
-    console.log(connection)
-    
     const sqlquery = 'SELECT * FROM dq_data';
 
-    console.log(sqlquery)
-    
     const [rows] = await connection.query(sqlquery);
 
     res.status(201).json([rows]);
